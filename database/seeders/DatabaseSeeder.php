64ceba45d614a96a4132122f59cf3c0e419c16ec
Muhammad-Ashfaq1 <mitf19e032@gmail.com>
2025-08-21 20:51:49 +0500

    Uncomment seeder calls in DatabaseSeeder for role permissions, default users, vehicle categories, and locations

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,12 +16,10 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
         $this->call([
-
-           // RolePermissionSeeder::class,
-           // DefaultUsersSeeder::class,
-            //VehicleCategorySeeder::class,
-            //LocationSeeder::class,
-
+            RolePermissionSeeder::class,
+            DefaultUsersSeeder::class,
+            VehicleCategorySeeder::class,
+            LocationSeeder::class,
             VehiclePartSeeder::class,
         ]);
     }
