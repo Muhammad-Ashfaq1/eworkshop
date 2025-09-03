db4b61ab9398fecc7571a863f9fa2d9edb0823be
Muhammad-Ashfaq1 <mitf19e032@gmail.com>
2025-09-03 19:07:04 +0500

    route prefix

--- routes/web.php
+++ routes/web.php
@@ -35,14 +35,16 @@ Route::post('updatePassword/{id}', [AuthController::class, 'updatePassword'])->m
 // Route::post('/reset-password', [PasswordController::class, 'resetPassword'])->name('reset.password');
 Route::get('/verify-user/{id}', [AuthController::class, 'verifyUser'])->name('verify.user');
 
-Route::get('get-towns', [DropdownController::class, 'getTowns'])->name('dropdown.getTowns');
-Route::get('get-vehicle-categories', [DropdownController::class, 'getVehicleCategories'])->name('dropdown.getVehicleCategories');
-Route::get('get-vehicles', [DropdownController::class, 'getVehicles'])->name('dropdown.getVehicles');
-Route::get('get-locations', [DropdownController::class, 'getLocations'])->name('dropdown.getLocations');
-Route::get('get-fleet-managers', [DropdownController::class, 'getFleetManagers'])->name('dropdown.getFleetManagers');
-Route::get('get-mvis', [DropdownController::class, 'getMvis'])->name('dropdown.getMvis');
-Route::get('get-defect-reports', [DropdownController::class, 'getDefectReports'])->name('dropdown.getDefectReports');
-Route::get('get-vehicle-parts', [DropdownController::class, 'getVehicleParts'])->name('dropdown.getVehicleParts');
+Route::controller(DropdownController::class)->name('dropdown.')->group(function () {
+    Route::get('get-towns', 'getTowns')->name('getTowns');
+    Route::get('get-vehicle-categories', 'getVehicleCategories')->name('getVehicleCategories');
+    Route::get('get-vehicles', 'getVehicles')->name('getVehicles');
+    Route::get('get-locations', 'getLocations')->name('getLocations');
+    Route::get('get-fleet-managers','getFleetManagers')->name('getFleetManagers');
+    Route::get('get-mvis', 'getMvis')->name('getMvis');
+    Route::get('get-defect-reports', 'getDefectReports')->name('getDefectReports');
+    Route::get('get-vehicle-parts', 'getVehicleParts')->name('getVehicleParts');
+});
 
 // Defect Reports routes with permission middleware
 Route::middleware(['auth'])->controller(DefectReportController::class)->prefix('defect-reports')->name('defect-reports.')->group(function () {
